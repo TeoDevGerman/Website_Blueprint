743958172555c2b393eef686eed9fed5db8b0624
Teoman W. <teoakateo@gmail.com>
2024-03-29 18:34:10 +0100

    feat: innit

---
+++ Website Blueprint/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        This is a website blueprint with React, TypeScript, Tailwind CSS, and Vite.
+    </h1>
+    </>
+  )
+}
+
+export default App
